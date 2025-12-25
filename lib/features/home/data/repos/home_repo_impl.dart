33a19b3a99c7e842b9173e248e80d9a2c5728b5c
abdelrahman1803 <abdelrahman.cis.eng@gmail.com>
2025-12-26 00:46:40 +0200

    implement fetchFeaturedBooks method in HomeRepoImpl

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,31 @@
 import 'package:bookly/core/errors/error_handler.dart';
+import 'package:bookly/features/home/data/data%20source/home_local_data_source.dart';
+import 'package:bookly/features/home/data/data%20source/home_remote_data_source.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 
   @override
