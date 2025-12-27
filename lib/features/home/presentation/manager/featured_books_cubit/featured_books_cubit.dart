4a3ea708f84fb364895884881d4fdbd497ec515c
abdelrahman1803 <abdelrahman.cis.eng@gmail.com>
2025-12-27 21:38:54 +0200

    replace bloc package to flutter_bloc

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/use%20cases/fetch_featured_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
--- lib/features/home/presentation/manager/latest_books_cubit/latest_books_cubit_cubit.dart
+++ lib/features/home/presentation/manager/latest_books_cubit/latest_books_cubit_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/use%20cases/fetch_latest_books_use_case.dart';
 import 'package:meta/meta.dart';
