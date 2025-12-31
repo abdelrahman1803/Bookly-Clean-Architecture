137cf3fb60b06a4c75412bd27ef2716ab5fe01e7
abdelrahman1803 <abdelrahman.cis.eng@gmail.com>
2025-12-31 16:07:38 +0200

    apply pagination to local data source

--- lib/features/home/data/data source/home_local_data_source.dart
+++ lib/features/home/data/data source/home_local_data_source.dart
@@ -4,7 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
-  List<BookEntity> fetchLatestBooks();
+  List<BookEntity> fetchLatestBooks({int pageNumber = 0});
 }
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
@@ -24,8 +24,17 @@ class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   }
 
   @override
-  List<BookEntity> fetchLatestBooks() {
+  List<BookEntity> fetchLatestBooks({int pageNumber = 0}) {
     var box = Hive.box<BookEntity>(kLatestBox);
-    return box.values.toList();
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10;
+    int length = box.length;
+    if (startIndex >= length) {
+      return [];
+    }
+    if (endIndex > length) {
+      endIndex = length;
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
   }
 }
